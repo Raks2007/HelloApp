081d0b38b459c027786bfc66cae9bd1e71ad299c
Raks2007 <rey.rak2120@gmail.com>
2026-03-17 17:37:47 +0000

    UC3-display-name-default

---
+++ HelloWorld3.java
@@ -0,0 +1,10 @@
+public class HelloWorld3 {
+
+    public static void main(String[] args) {
+
+        // Using ternary operator
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
